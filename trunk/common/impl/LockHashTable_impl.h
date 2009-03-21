68352712d33ffe908127a755251872fc918bf676
daidodo <daidodo@cbd2cf98-a58d-11dd-91bf-632da8b6e10b>
2009-03-21 03:42:16 +0000

    修正hashtable的Iterate，返回桶内所有元素的数组

--- trunk/common/impl/LockHashTable_impl.h
+++ trunk/common/impl/LockHashTable_impl.h
@@ -6,8 +6,11 @@
         __hashT_node_impl
         __hash_table_read_pointer
         __hash_table_write_pointer
+    History:
+        20090321    修改__hash_table_pointer_base包含__hashT_node_impl指针，而不是__hashT_node_impl::data_的指针
 //*/
 
+#include <vector>
 #include <common/Mutex.h>
 
 NS_IMPL_BEGIN
@@ -26,7 +29,8 @@ struct __hashT_node_impl
 template<class Value,class LockT>
 class __hash_table_pointer_base
 {
-    typedef __hash_table_pointer_base<Value,LockT> __Myt;
+    typedef __hash_table_pointer_base<Value,LockT>  __Myt;
+    typedef __hashT_node_impl<Value>                __NodePtr;
     typedef void (__Myt::*__SafeBool)();
 protected:
     typedef LockT *             __LockPtr;
@@ -46,7 +50,7 @@ private:
     __hash_table_pointer_base(const __Myt &);
     __Myt & operator =(const __Myt &);
 protected:
-    Value *     pv_;
+    __NodePtr   pv_;
     __LockPtr   pl_;
 };
 
@@ -58,8 +62,8 @@ class __hash_table_read_pointer : public __hash_table_pointer_base<Value,LockT>
 public:
     typedef const Value * pointer;
     typedef const Value & reference;
-    pointer operator ->() const{return __MyBase::pv_;}
-    reference operator *() const{return *__MyBase::pv_;}
+    pointer operator ->() const{return &operator ->();}
+    reference operator *() const{return __MyBase::pv_->data_;}
 };
 
 template<class Value,class LockT,class KeyOfValue,template<typename>class EqualKey,template<typename>class Hash,class Alloc>
@@ -70,8 +74,64 @@ class __hash_table_write_pointer : public __hash_table_pointer_base<Value,LockT>
 public:
     typedef Value * pointer;
     typedef Value & reference;
-    pointer operator ->() const{return __MyBase::pv_;}
-    reference operator *() const{return *__MyBase::pv_;}
+    pointer operator ->(){return &operator ->();}
+    reference operator *(){return __MyBase::pv_->data_;}
+};
+
+template<class Value,class LockT>
+class __hash_table_elem_array_base
+{
+    typedef __hash_table_elem_array_base<Value,LockT>   __Myt;
+    typedef __hashT_node_impl<Value>    __NodePtr;
+    typedef __DZ_VECTOR(__NodePtr)      __Array;
+protected:
+    typedef LockT *             __LockPtr;
+    typedef CLockAdapter<LockT> __Adapter;
+    __hash_table_elem_array_base():pl_(0){}
+    ~__hash_table_elem_array_base(){release();}
+public:
+    void release(){
+        if(pl_)
+            __Adapter().Unlock(*pl_);
+        pl_ = 0;
+        ar_.clear();
+    }
+    size_t size() const{return ar_.size();}
+    bool empty() const{return ar_.empty();}
+private:
+    __hash_table_elem_array_base(const __Myt &);
+    __Myt & operator =(const __Myt &);
+protected:
+    __Array     ar_;
+    __LockPtr   pl_;
+};
+
+template<class Value,class LockT,class KeyOfValue,template<typename>class EqualKey,template<typename>class Hash,class Alloc>
+class __hash_table_read_elem_array : public __hash_table_elem_array_base<Value,LockT>
+{
+    friend class CLockHashTable<Value,LockT,KeyOfValue,EqualKey,Hash,Alloc>;
+    typedef __hash_table_elem_array_base<Value,LockT>  __MyBase;
+public:
+    typedef const Value * pointer;
+    typedef const Value & reference;
+    reference operator [](size_t index) const{
+        assert(__MyBase::ar_[index]);
+        return __MyBase::ar_[index]->data_;
+    }
+};
+
+template<class Value,class LockT,class KeyOfValue,template<typename>class EqualKey,template<typename>class Hash,class Alloc>
+class __hash_table_write_elem_array : public __hash_table_elem_array_base<Value,LockT>
+{
+    friend class CLockHashTable<Value,LockT,KeyOfValue,EqualKey,Hash,Alloc>;
+    typedef __hash_table_elem_array_base<Value,LockT>  __MyBase;
+public:
+    typedef Value * pointer;
+    typedef Value & reference;
+    reference operator [](size_t index){
+        assert(__MyBase::ar_[index]);
+        return __MyBase::ar_[index]->data_;
+    }
 };
 
 NS_IMPL_END
