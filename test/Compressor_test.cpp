93405de75779612afb2240120442b5e9b918a072
daidodo <daidodo@cbd2cf98-a58d-11dd-91bf-632da8b6e10b>
2011-10-30 03:03:19 +0000

    mod test

--- test/Compressor_test.cpp
+++ test/Compressor_test.cpp
@@ -3,13 +3,14 @@
 #include <iostream>
 
 #include <common/CompressorLZO.h>
+#include <common/CompressorQuickLZ.h>
 
 using namespace std;
 using namespace NS_SERVER;
 
 int main()
 {
-    string origin("1234567890abcdefghijklmnopeaibanadj;3apU(#JDFNPDdbdn(NNEndjafefJJENFbn apfa3 j[f asdfas]a\3a0fa sdjfna3 nadka.fj3jadshadsfa");
+    string origin("1234567890abcdefghijklmnopeaibanadj;3apU(#JDFNPDdbdn(NNEndjafefJJENFbn apfa3 j[f asdfas]a\\3a0fa sdjfna3 nadka.fj3jadshadsfa");
     CCompressorLZO comp;
     string encry;
     int i = comp.Compress(origin, encry);
