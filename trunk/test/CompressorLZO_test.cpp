c7bae9dc7effd6cb167d1bac9e9d6e9409fa70d6
daidodo <daidodo@cbd2cf98-a58d-11dd-91bf-632da8b6e10b>
2011-10-30 03:00:58 +0000

    add test

---
+++ trunk/test/CompressorLZO_test.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include <common/CompressorLZO.h>
+
+using namespace std;
+using namespace NS_SERVER;
+
+int main()
+{
+    string origin("1234567890abcdefghijklmnopeaibanadj;3apU(#JDFNPDdbdn(NNEndjafefJJENFbn apfa3 j[f asdfas]a\3a0fa sdjfna3 nadka.fj3jadshadsfa");
+    CCompressorLZO comp;
+    string encry;
+    int i = comp.Compress(origin, encry);
+    if(0 != i){
+        cerr<<"CCompressorLZO::Compress(string) returns "<<i<<endl;
+        return 1;
+    }
+    string decry;
+    i = comp.Decompress(encry, decry);
+    if(0 != i){
+        cerr<<"CCompressorLZO::Decompress(string) returns "<<i<<endl;
+        return 1;
+    }
+    if(origin != decry){
+        cerr<<"origin="<<Tools::Dump(origin)<<" is diff from decry="<<Tools::Dump(decry)<<endl;
+        return 1;
+    }
+    cout<<"CCompressorLZO test succ\n";
+    return 0;
+}
