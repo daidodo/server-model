8a9c9ddd78ba79eb720fb31274839fb57b1e06f4
daidodo <daidodo@cbd2cf98-a58d-11dd-91bf-632da8b6e10b>
2011-11-12 15:13:40 +0000

    fix bugs; add test

---
+++ test/Heap_test.cpp
@@ -0,0 +1,51 @@
+#include <common/Heap.h>
+
+#include "comm.h"
+
+#define MAX_VAL     100000
+#define HEAP_SZ     1000
+
+static bool testHeap()
+{
+    CHeap<int> heap;
+    CFixedHeap<int> fixheap(HEAP_SZ / 10);
+    for(int i = 0;i < HEAP_SZ;++i){
+        int v = rand() % MAX_VAL;
+        heap.push(v);
+        fixheap.push(v);
+    }
+    if(heap.size() != HEAP_SZ){
+        cerr<<"heap.size()="<<heap.size()<<" is not "<<HEAP_SZ<<endl;
+        return false;
+    }
+    if(fixheap.size() != fixheap.max_size()){
+        cerr<<"fixheap.size()="<<fixheap.size()<<" is not fixheap.max_size()="<<fixheap.max_size()<<endl;
+        return false;
+    }
+    for(int prev = MAX_VAL;!heap.empty();heap.pop()){
+        int cur = heap.top();
+        if(prev < cur){
+            cerr<<"cur="<<cur<<" > prev="<<prev<<" while heap.size()="<<heap.size()<<endl;
+            return false;
+        }
+        if(heap.size() == fixheap.size()){
+            int fcur = fixheap.top();
+            if(cur != fcur){
+                cerr<<"heap.top()="<<cur<<" != fixheap.top()="<<fcur<<" while fixheap.size()="<<fixheap.size()<<endl;
+                return false;
+            }
+            fixheap.pop();
+        }
+        prev = cur;
+    }
+    return true;
+}
+
+int main()
+{
+    srand(time(0));
+    if(!testHeap())
+        return false;
+    cout<<"Heap test succ\n";
+    return 0;
+}
