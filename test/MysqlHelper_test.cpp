da3193655086b79da25cd4e065e5fc3984bfc24b
daidodo <daidodo@cbd2cf98-a58d-11dd-91bf-632da8b6e10b>
2011-12-13 13:54:38 +0000

    add test

--- test/MysqlHelper_test.cpp
+++ test/MysqlHelper_test.cpp
@@ -5,12 +5,40 @@
 static bool testMySQL()
 {
     CMySQL mysql;
+    //connect
     mysql.Connect("localhost", "test", "test@pass", "test");
+    if(!mysql.IsConnected() || !mysql){
+        cerr<<"Connect() failed\n";
+        return false;
+    }
+    if(!mysql.Ping()){
+        cerr<<"Ping() failed, errno="<<mysql.ErrorNo()<<": "<<mysql.ErrorMsg()<<endl;
+        return false;
+    }
+    if(!mysql.SelectDB("test")){
+        cerr<<"SelectDB('test') failed, errno="<<mysql.ErrorNo()<<": "<<mysql.ErrorMsg()<<endl;
+        return false;
+    }
+    //create table
+    const char * sql = "create temporary table `test_table` (`name` varchar(16) not null, `value` int default 0, primary key(`name`))";
+    if(!mysql.Query(sql)){
+        cerr<<"Query(create table sql) failed, errno="<<mysql.ErrorNo()<<": "<<mysql.ErrorMsg()<<endl;
+        return false;
+    }
+    int i;
+    if(0 != (i = mysql.AffectedRows())){
+        cerr<<"AffectedRows()="<<i<<" is not 0, errno="<<mysql.ErrorNo()<<": "<<mysql.ErrorMsg()<<endl;
+        return false;
+    }
+    //insert
+
     return true;
 }
 
 int main()
 {
+    if(!testMySQL())
+        return 1;
     cout<<"MysqlHelper test succ\n";
     return 0;
 }
