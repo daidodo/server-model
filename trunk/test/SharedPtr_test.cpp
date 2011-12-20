dd9361cbd10872945c700e6bbcfcc668eb5a3f87
daidodo <daidodo@cbd2cf98-a58d-11dd-91bf-632da8b6e10b>
2011-12-20 13:40:44 +0000

    add test

---
+++ trunk/test/SharedPtr_test.cpp
@@ -0,0 +1,90 @@
+#include <common/SharedPtr.h>
+
+#include "comm.h"
+
+struct CTest
+{
+    typedef __DZ_ALLOC<CTest> allocator_type;
+    static int cnt;
+    CTest(){++cnt;}
+    explicit CTest(const __DZ_STRING & s):s_(s){++cnt;}
+    CTest(const CTest & a):s_(a.s_){++cnt;}
+    ~CTest(){--cnt;}
+    static CTest * GetObject(){
+        CTest * ret = allocator_type().allocate(1);
+        return new (ret) CTest;
+    }
+    static CTest * GetObject(const __DZ_STRING & s){
+        CTest * ret = allocator_type().allocate(1);
+        return new (ret) CTest(s);
+    }
+    __DZ_STRING s_;
+};
+
+int CTest::cnt = 0;
+
+static bool testSharedPtr()
+{
+    typedef CSharedPtr<CTest> __Ptr;
+    if(0 != CTest::cnt){
+        cerr<<"1: CTest::cnt="<<CTest::cnt<<" is not 0\n";
+        return false;
+    }
+    for(int i = 0;i < 100;++i){
+        __Ptr p1 = CTest::GetObject();
+        if(!p1){
+            cerr<<"p1 == NULL\n";
+            return false;
+        }
+        p1->s_ = "abcdef";
+        if((*p1).s_ != "abcdef"){
+            cerr<<"(*p1).s_='"<<(*p1).s_<<"' is not 'abcdef'\n";
+            return false;
+        }
+        __Ptr p2 = p1;
+        if(p2 != p1){
+            cerr<<"p2 != p1\n";
+            return false;
+        }
+        if(p2->s_ != "abcdef"){
+            cerr<<"p2->s_='"<<p2->s_<<"' is not 'abcdef'\n";
+            return false;
+        }
+        __Ptr p3 = CTest::GetObject("abcdef");
+        if(p3 == p1){
+            cerr<<"p3 == p1\n";
+            return false;
+        }
+        if(p3->s_ != "abcdef"){
+            cerr<<"p3->s_='"<<p3->s_<<"' is not 'abcdef'\n";
+            return false;
+        }
+        if(p3 != &(*p3)){
+            cerr<<"p3 != &(*p3)\n";
+            return false;
+        }
+        if(p3 == &(*p1)){
+            cerr<<"p3 == &(*p1)\n";
+            return false;
+        }
+        p1.swap(p3);
+        p1 = 0;
+        if(p1){
+            cerr<<"p1 != NULL\n";
+            return false;
+        }
+    }
+    if(0 != CTest::cnt){
+        cerr<<"2: CTest::cnt="<<CTest::cnt<<" is not 0\n";
+        return false;
+    }
+    return true;
+}
+
+int main()
+{
+    if(!testSharedPtr())
+        return 1;
+    cout<<"SharedPtr test succ\n";
+    return 0;
+}
