62439fb88938f8bd30899c742e54abec0d1ae2ca
daidodo <daidodo@cbd2cf98-a58d-11dd-91bf-632da8b6e10b>
2012-01-18 09:46:38 +0000

    add CmdHandler

---
+++ branches/daidodo-MultiAcceptServer/server/frame/CmdHandler.h
@@ -0,0 +1,41 @@
+#ifndef DOZERG_CMD_HANDLER_H_20120118
+#define DOZERG_CMD_HANDLER_H_20120118
+
+#include <Threads.h>
+
+#include "Structs.h"
+
+NS_SERVER_BEGIN
+
+struct CHandlerCtorParams
+{
+    size_t stackSize_;
+    __FdEventQue & addingQue_;
+    __FdEventQue & eventQue_;
+    __FdSockMap & fdSockMap_;
+    __QueryCmdQue & queryCmdQue_;
+};
+
+struct CHandlerInitParams
+{
+    int threadCountMax_;
+};
+
+class CCmdHandler : public CThreadManager<__QueryCmdQue>
+{
+    typedef CThreadManager<__QueryCmdQue> __MyBase;
+    explicit CCmdHandler(const CHandlerCtorParams & params);
+    bool Init(const CHandlerInitParams & params);
+protected:
+    virtual void doIt(__Job & job);
+private:
+    //members
+    __FdEventQue & addingQue_;
+    __FdEventQue & eventQue_;
+    __FdSockMap & fdSockMap_;
+};
+
+NS_SERVER_END
+
+#endif
+
