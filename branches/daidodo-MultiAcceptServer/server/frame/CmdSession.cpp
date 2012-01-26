1aed9997f1d74f9549b8868aea008ba008fcd145
daidodo <daidodo@cbd2cf98-a58d-11dd-91bf-632da8b6e10b>
2012-01-26 08:16:06 +0000

    decode cmd

---
+++ branches/daidodo-MultiAcceptServer/server/frame/CmdSession.cpp
@@ -0,0 +1,33 @@
+#include "CmdSession.h"
+
+NS_SERVER_BEGIN
+
+CCmdSession * CCmdSession::GetObject(__SockPtr & sock, __CmdBase * cmd, CSockAddr & udpClientAddr)
+{
+    CCmdSession * ret = allocator_type().allocate(1);
+    return new (ret) CCmdSession(sock, cmd, udpClientAddr);
+}
+
+CCmdSession::CCmdSession(__SockPtr & sock, __CmdBase * cmd, CSockAddr & udpClientAddr)
+    : sock_(sock)
+    , cmd_(cmd)
+{
+    udpClientAddr_.swap(udpClientAddr);
+}
+
+CCmdSession::~CCmdSession()
+{
+    if(cmd_){
+        assert(sock_);
+        sock_->ReleaseCmd(cmd_);
+        cmd_ = 0;
+    }
+    sock_ = 0;
+}
+
+std::string CCmdSession::ToString() const
+{
+    return "";
+}
+
+NS_SERVER_END
---
+++ branches/daidodo-MultiAcceptServer/server/frame/Command.cpp
@@ -0,0 +1,110 @@
+#include <sstream>
+
+#include <Logger.h>
+#include "Command.h"
+
+NS_SERVER_BEGIN
+
+//struct CCmdBase
+
+__OnDataArriveRet CCmdBase::OnDataArrive(const char * buf, size_t sz)
+{
+    assert(buf);
+    if(sz < 5)
+        return __OnDataArriveRet(RR_NEED_MORE, 5 - sz);
+    U16 totalLen = 0;
+    CInByteStream in(buf, sz);
+    in>>totalLen;
+    if(totalLen > 1024)
+        return __OnDataArriveRet(RR_ERROR, 0);
+    if(sz >= totalLen)
+        return __OnDataArriveRet(RR_COMPLETE, sz - totalLen);
+    return __OnDataArriveRet(RR_NEED_MORE, totalLen - sz);
+}
+
+//wLen cStx wCmdId body cEtx
+CCmdBase * CCmdBase::DecodeCmd(const char * buf, size_t sz)
+{
+    LOCAL_LOGGER(logger, "CCmdBase::DecodeCmd");
+    assert(buf && sz);
+    CInByteStream in(buf, sz);
+    //validate
+    U16 totalLen = 0;
+    U8 stx = 0;
+    U16 cmdId = 0;
+    if(!(in>>totalLen>>stx>>cmdId)){
+        ERROR("decode buf="<<Tools::Dump(buf, sz)<<" error");
+        return 0;
+    }
+    U8 etx = buf[sz - 1];
+    if(sz != totalLen
+            || STX != stx
+            || ETX != etx){
+        ERROR("check totalLen="<<totalLen<<", stx="<<(int)stx<<", etx="<<(int)etx<<" failed for buf="<<Tools::Dump(buf, sz));
+        return 0;
+    }
+    //decode
+    switch(cmdId){
+        case CMD_QUERY:{
+            CCmdQuery * ret = new CCmdQuery;
+            if(!ret->Decode(in)){
+                ERROR("decode CMD_QUERY failed for buf="<<Tools::Dump(buf, sz));
+                return 0;
+            }
+            return ret;}
+        default:;
+    }
+    ERROR("cmdId="<<cmdId<<" is invalid in buf="<<Tools::Dump(buf, sz));
+    return 0;
+}
+
+void CCmdBase::ReleaseCmd(CCmdBase * cmd)
+{
+    assert(cmd);
+    switch(cmd->CmdId()){
+        case CMD_QUERY:delete cmd;break;
+        default:break;
+    }
+}
+
+std::string CCmdBase::ToString() const{
+    std::ostringstream oss;
+    switch(cmdId_){
+        case CMD_QUERY:oss<<"CMD_QUERY";break;
+        case CMD_RESP:oss<<"CMD_RESP";break;
+        default:oss<<"UNKNOWN["<<cmdId_<<"]";break;
+    }
+    return oss.str();
+}
+
+//struct CCmdQuery
+
+bool CCmdQuery::Decode(CInByteStream & in)
+{
+    return (in>>ver_
+        >>echo_);
+}
+
+std::string CCmdQuery::ToString() const
+{
+    std::ostringstream oss;
+    oss<<"{base="<<CCmdBase::ToString()
+        <<", ver_="<<ver_
+        <<", echo_="<<Tools::Dump(echo_)
+        <<"}";
+    return oss.str();
+}
+
+//struct CCmdResp
+
+std::string CCmdResp::ToString() const
+{
+    std::ostringstream oss;
+    oss<<"{base="<<CCmdBase::ToString()
+        <<", ver_="<<ver_
+        <<", result_="<<Tools::Dump(result_)
+        <<"}";
+    return oss.str();
+}
+
+NS_SERVER_END
