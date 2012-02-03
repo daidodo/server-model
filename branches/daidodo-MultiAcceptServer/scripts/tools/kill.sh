550d6fa4946577a8feff9cad144dbeb3ba6ba1b7
daidodo <daidodo@cbd2cf98-a58d-11dd-91bf-632da8b6e10b>
2012-02-03 18:02:43 +0000

    mod sh

--- branches/daidodo-MultiAcceptServer/scripts/tools/kill.sh
+++ branches/daidodo-MultiAcceptServer/scripts/tools/kill.sh
@@ -1,27 +1,40 @@
 #!/bin/sh
 
+if [ $# -lt 1 ] ; then
+  echo "Usage: $0 EXE [EXE2 ...]"
+  exit 1
+fi
+
 PWD=`pwd`
 BIN=server.out
-CMD=${PWD}/${BIN}
 
-PS_RECORD=`ps -ef | grep -v grep | grep "${CMD}"`
-PS_PID=`echo "${PS_RECORD}" | awk '{print $2}'`
+kill_server()
+{
+  CMD=${PWD}/$1
 
-if [ "${PS_RECORD}" = "" ]; then
+  PS_RECORD=`ps -ef | grep -v grep | grep "${CMD}"`
+  PS_PID=`echo "${PS_RECORD}" | awk '{print $2}'`
+
+  if [ "${PS_RECORD}" = "" ]; then
     echo "# ${BIN} is NOT running"
-else
-    kill -9 ${PS_PID}
-    
+  else
+    kill ${PS_PID}
+
     sleep 1
 
     PS_CHECK=`ps -ef | grep -v grep | grep "${CMD}"`
     if [ "${PS_CHECK}" = "" ]; then
-        echo "# ${BIN} stopped"
-        echo "${PS_RECORD}"
-        exit 0
+      echo "# ${BIN} stopped"
+      echo "${PS_RECORD}"
+      exit 0
     else
-        echo "# killing ${BIN} FAILED"
-        echo "${PS_CHECK}"
-        exit -1
+      echo "# killing ${BIN} FAILED"
+      echo "${PS_CHECK}"
+      exit -1
     fi
-fi
+  fi
+}
+
+for BIN in "$*" ; do
+  kill_server $BIN
+done
